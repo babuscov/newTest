27cde4f75a02a2643dae09a0f2dbf9f9533b1be4
babuscov <babuscovpetru@yahoo.com>
2020-02-11 15:59:55 +0100

    new1 commit

--- src/testGit.java
+++ src/testGit.java
@@ -2,5 +2,6 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println ("test git");
         System.out.println ("new");
+        System.out.println ("new1");
     }
 }
